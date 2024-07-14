7689f809c2d236bb6cc6896830b53bc2a45e1988
Sahitya Singh <166967863+git-Sahitya@users.noreply.github.com>
2024-07-14 03:33:49 +0000

    datatypes

---
+++ Basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 455436;
+let accountEmail = "Sahitya76464@gmail.com";
+var accountPassword = "3466337";
+accountCity = "Varanasi";
+
+
+accountEmail = "rfrfgf@gmailcom";
+accountPassword = "366466";
+accountCity = "Banaras";
+
+console.log(accountId);
