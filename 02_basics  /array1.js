bf690521fc6aed5ec0b3310a854a76509e8753c2
Sahitya Singh <166967863+git-Sahitya@users.noreply.github.com>
2024-07-17 05:25:18 +0000

    add

--- 02_basics  /array1.js
+++ 02_basics  /array1.js
@@ -1,10 +1,1 @@
- const marvel_hero = ["Ironman" , "Thor","Hulk" , "Captain America"]
-
- const dc_hero =["Superman","Batman","Aquaman","Flash"]
-
- const all_hero = marvel_hero.concat(dc_hero)
-
- console.log(all_hero);
-
- console.log("hey");
-
+ console.log("hey")
