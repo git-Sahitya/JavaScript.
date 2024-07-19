cfe82ba99862959e56d7e7090029bba5a735f43b
Sahitya Singh <166967863+git-Sahitya@users.noreply.github.com>
2024-07-19 04:26:22 +0000

    function

--- 03_basics/function.js
+++ 03_basics/function.js
@@ -51,3 +51,41 @@ function just_logged_in (user_name = "JOJO"){
 }
 
 console.log(just_logged_in("Sahitya"));
+
+// Type--6  Rest_Operator
+
+function calculataCartPrice (...num1){
+    return num1
+}
+
+console.log(calculataCartPrice(200,400,500,1000));
+
+// Type--7 Object in Function
+
+const user1 = {
+    name : "Sahitya",
+    age : 23,
+    email : "Sahitya@google.com"
+}
+
+function handleObject (anyObject){
+
+     console.log(`User name is ${anyObject.name} and age is ${anyObject.age} and there email id is ${anyObject.email}`);
+}
+
+//handleObject(user1);
+
+handleObject({
+    name : "JOJO",
+    age : "1.5 year",
+    email : "jojolal@doggo.com"
+})
+// Type--8  function for array
+
+const myNewArray = [200,400,600,800,1000]
+
+function returnSecondValue (...getArray){
+    return  getArray[0]
+}
+
+console.log(returnSecondValue(myNewArray));
