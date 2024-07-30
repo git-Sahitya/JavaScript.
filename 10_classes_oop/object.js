e8b4bbd46b1b7274937bedff8e583f86e2f32db3
Sahitya Singh <166967863+git-Sahitya@users.noreply.github.com>
2024-07-30 12:35:55 +0000

    prototype

---
+++ 10_classes_oop/object.js
@@ -0,0 +1,10 @@
+function multipleBy5 (num){
+ 
+     return num*5
+}
+
+// multipleBy5.power = 2
+
+console.log(multipleBy5(8));
+console.log(multipleBy5.powew);
+console.log(multipleBy5.prototype);
